5dcdff1eb3fa0254b4d4d49883750aced585a777
mtrepanier935 <MichaelMDTrep@Gmail.com>
2020-09-08 17:46:19 -0400

    second commit

--- copyspecial.py
+++ copyspecial.py
@@ -9,62 +9,57 @@
 # give credits
 __author__ = "Michael Trepanier"
 
-import re
+# import re
 import sys
 import shutil
-import subprocess
+# import subprocess
 import argparse
-import os.path
+import os
+# from zipfile import ZipFile
 # from os import listdir
 
 
-def get_special_paths(dirs):
+def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
 
     special_files = []
-    print(dirs)
-    for dirname in dirs:
-        file_list = os.listdir(dirname)
-        # print(file_list)
-        for file in file_list:
-            # print(file)
-            parts = file.split('__')
-            # print(parts)
-            if (len(parts) >= 3):
-                print(file)
-                if (file in special_files):
-                    print(file+" is a duplicate")
-                    sys.exit(1)
-                else:
-                    special_files.append(file)
+    dirname = os.path.abspath(dirname)
+    file_list = os.listdir(dirname)
+    for file in file_list:
+        parts = file.split('__')
+        if (len(parts) >= 3):
 
-    print(special_files)
+            special_files.append(dirname+'/'+file)
+
+    # print(special_files)
     return special_files
 
 
 def copy_to(path_list, dest_dir):
+    """This copies to a new directory"""
 
-    path_list = os.listdir("/home/")
-    dest_dir = "/home/mtrepanier935/kenzie-q3/Copy-Special"
-    for files in path_list:
-        if files.endswith(".txt"):
-            shutil.copy(path_list, dest_dir)
-
-###########
-    shutil.copyfile(path_list, dest_dir)
-    print(path_list)
-
-    return
+    os.makedirs(dest_dir, exist_ok=True)
+    if not os.path.isdir(dest_dir):
+        print(f"{dest_dir} is NOT a directory")
+        sys.exit(1)
+    for f in path_list:
+        shutil.copy(f, dest_dir)
 
 
 def zip_to(path_list, dest_zip):
+    """This zips files with the destination directory"""
+    cmd_str = 'zip -j '+dest_zip
+    for file in path_list:
+        cmd_str = cmd_str+' '+file
 
-    print(f"Command i'm going to do: \nzip -j {dest_zip}")
+    # print('Command I am going to do:')
+    print(cmd_str)
+    # os.system(cmd_str)
+    stream = os.popen(cmd_str)
+    output = stream.read()
+    # subprocess.run(cmd_str)
 
-    command_list = ['zip', '-j', dest_zip]
-    subprocess.run(
-        command_list,
-    )
+    print(output)
     return
 
 
@@ -74,13 +69,21 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    parser.add_argument('dirs', help='directory(s) to search', nargs='+')
+    parser.add_argument('dirs', help='directory(s) to search')
     # TODO: add one more argument definition to parse the 'from_dir' argument
     ns = parser.parse_args(args)
-    print(ns)
 
-    if (ns.dirs != None):
-        get_special_paths(ns.dirs)
+    # print(ns)
+
+    if ns.dirs is not None:
+
+        special_paths = get_special_paths(ns.dirs)
+        print("\n".join(special_paths))
+    if ns.todir is not None:
+        copy_to(special_paths, ns.todir)
+
+    if ns.tozip is not None:
+        zip_to(special_paths, ns.tozip)
 
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
